1e0dda06fc1c15c1b8ff20b9e3d37efc2fe8cdde
ALEXANDRE DE ABREU AMARAL <al.alexandre.amaral@impatech.edu.br>
2026-09-16 15:30:46 -0300

    Aula 1 - cria módulo MYTOOLS

---
+++ MYTOOLS/__init__.py
@@ -0,0 +1,17 @@
+PI_INT = "1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679"
+
+E_INT = "7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274"
+
+
+def pi_real(N):
+    if N <= 0 or N >= 100:
+        return "N deve ser um número natural entre 1 e 99"
+
+    return "3," + PI_INT[:N]
+
+
+def e_real(N):
+    if N <= 0 or N >= 100:
+        return "N deve ser um número natural entre 1 e 99"
+
+    return "2," + E_INT[:N]
